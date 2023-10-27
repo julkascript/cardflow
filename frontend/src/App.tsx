fc0c1dee4c7507a5ae4925cce1262c215a76b61d
RyotaMitaraiWeb <Titanium5624@gmail.com>
2023-10-27 17:48:36 +0300

    add TailwindCSS

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -26,7 +26,8 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Click on the Vite and React logos to learn more. 
+        <span className="font-bold">Tailwind test</span>
       </p>
     </>
   )
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
+import { Plugin, defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import tailwindcss from 'tailwindcss';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  /* 
+    TS compiler complains about the TailwindCSS plugin, so
+    providing a bandaid fix for the time being
+  */
+  plugins: [react(), tailwindcss() as unknown as Plugin],
 })
