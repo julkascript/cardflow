10e53ca1816c1ac850670ea2f28eb061c5ed8b54
RyotaMitaraiWeb <Titanium5624@gmail.com>
2023-10-27 17:54:05 +0300

    add MUI

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Button } from '@mui/material'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -29,6 +30,7 @@ function App() {
         Click on the Vite and React logos to learn more. 
         <span className="font-bold">Tailwind test</span>
       </p>
+      <Button>Test Button</Button>
     </>
   )
 }
