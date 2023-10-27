558e097ea80532acddfd5c15d83fbe32f9263343
RyotaMitaraiWeb <Titanium5624@gmail.com>
2023-10-27 19:01:07 +0300

    simplify the bandaid fix on vite plugins

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,14 +1,14 @@
-import { Plugin, defineConfig } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from 'tailwindcss';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   /* 
-    TS compiler complains about the TailwindCSS plugin, so
+    TS compiler complains about some of the plugins, so
     providing a bandaid fix for the time being
   */
-  plugins: [react(), tailwindcss() as unknown as Plugin],
+  plugins: [react(), tailwindcss()],
   test: {
     environment: 'jsdom',
   },
