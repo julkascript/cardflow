359edd007532bbffd5aed90df9971a11b8cc64b4
RyotaMitaraiWeb <Titanium5624@gmail.com>
2023-10-28 18:15:49 +0300

    apply formatting rules to current files

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 import tailwindcss from 'tailwindcss';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  /* 
+  /*
     TS compiler complains about some of the plugins, so
     providing a bandaid fix for the time being
   */
