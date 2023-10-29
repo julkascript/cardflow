06306d171ceb21053ee260152ec5720522fef843
Milen Palachorov <feniks9511@gmail.com>
2023-10-29 17:55:26 +0200

    added url files for each app
    created urls.py for each applications in apps and added all to the main urls file

--- backend/cardflow/cardflow/urls.py
+++ backend/cardflow/cardflow/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('apps.accounts.urls')),
+    path('', include('apps.card.urls')),
+    path('', include('apps.user_profile.urls')),
 ]
+
