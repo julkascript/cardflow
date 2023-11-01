6f4b38093c94553df28245ec09106146bea92d41
Simeon Todorov <59865649+dinocom33@users.noreply.github.com>
2023-11-01 18:09:20 +0200

    Change rarity field in YugiohCardInSet

--- backend/yugiohcardinset/models.py
+++ backend/yugiohcardinset/models.py
@@ -2,7 +2,10 @@ from django.db import models
 
 
 class YugiohCardInSet(models.Model):
-    rarity = models.CharField(max_length=100)
+    rarity = models.ForeignKey(
+        'yugiohcardrarity.YugiohCardRarity',
+        on_delete=models.CASCADE,
+    )
     set = models.CharField(max_length=100)
 
     def __str__(self):
