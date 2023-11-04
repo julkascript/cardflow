e761b79f70f95bdd4d461cdbb51a3340f31cfbcf
Simeon Todorov <59865649+dinocom33@users.noreply.github.com>
2023-11-04 11:32:07 +0200

    Change some fields and DB relations

--- backend/yugiohcardrarity/admin.py
+++ backend/yugiohcardrarity/admin.py
@@ -5,6 +5,6 @@ from yugiohcardrarity.models import YugiohCardRarity
 
 @admin.register(YugiohCardRarity)
 class YugiohCardRarityAdmin(admin.ModelAdmin):
-    list_display = ('set', 'rarity')
-    list_filter = ('set', 'rarity')
-    search_fields = ('set', 'rarity')
+    list_display = ('rarity',)
+    list_filter = ('rarity',)
+    search_fields = ('rarity',)
