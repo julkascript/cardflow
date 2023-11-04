28065788b766e6e5d917ced8c0fa78f0237012f7
Simeon Todorov <59865649+dinocom33@users.noreply.github.com>
2023-11-04 15:28:41 +0200

    Update admin.py
    Updated YugiohCardInSet admin.py

--- backend/yugiohcardinset/admin.py
+++ backend/yugiohcardinset/admin.py
@@ -6,6 +6,7 @@ from yugiohcardinset.models import YugiohCardInSet
 @admin.register(YugiohCardInSet)
 class YugiohCardInSetAdmin(admin.ModelAdmin):
     list_display = ('set_name', 'rarity_name')
+    list_filter = 'set__card_set_name', 'rarity__rarity'
 
     def set_name(self, obj):
         return obj.set.card_set_name
