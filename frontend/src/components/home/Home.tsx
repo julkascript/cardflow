df880273d7433d0946ef6ca7413826d29c82affa
Kaloyan <kthristow@gmail.com>
2023-11-08 18:54:17 +0200

    Update the home component
    Add additional styles for the buttons.

--- frontend/src/components/home/Home.tsx
+++ frontend/src/components/home/Home.tsx
@@ -5,9 +5,9 @@ function Home(): JSX.Element {
   return (
     <div className="flex justify-center items-center flex-col">
       <div className="flex">
-        <h1 className="text-9xl font-extrabold px-4 py-2">Buy.</h1>
-        <h1 className="text-9xl font-extrabold px-4 py-2">Sell.</h1>
-        <h1 className="text-9xl font-extrabold px-4 py-2">Trade.</h1>
+        <h1 className="text-9xl font-extrabold px-4 py-2 text-[#007FEF]">Buy.</h1>
+        <h1 className="text-9xl font-extrabold px-4 py-2 text-[#7E27C7]">Sell.</h1>
+        <h1 className="text-9xl font-extrabold px-4 py-2 text-[#FF544B]">Trade.</h1>
       </div>
       <p className="text-lg mt-2 font-normal text-inter text-[#6F6F6F] mt-2">
         Cardflow provides you with the tools and infrastructure
@@ -15,20 +15,24 @@ function Home(): JSX.Element {
       <p className="text-lg mt-2 font-normal text-inter text-[#6F6F6F] mt-2">
         to move your cards from A to B.
       </p>
-      <div className="flex mt-2">
-        <Button
-          variant="contained"
-          startIcon={<Logo color='white' size={33} />}
-          className="bg-blue-500 text-white font-semibold py-2 px-4 rounded-lg mr-2"
-        >
-          Start exploring
-        </Button>
-        <Button
-          variant="contained"
-          className="bg-green-500 text-white font-semibold py-2 px-4 rounded-lg"
-        >
-          Sign In
-        </Button>
+      <div className="flex mt-2 pt-20">
+        <div className="pr-40">
+          <Button
+            variant="contained"
+            sx={{ color: 'white', padding: '10px', width: 210, textTransform:"none" }}
+            startIcon={<Logo color="white" size={20}/>}
+          >
+            Start exploring
+          </Button>
+        </div>
+        <div>
+          <Button
+            variant="contained"
+            sx={{ color: 'black', backgroundColor: 'white', width: 210, padding: '10px', textTransform:"none" }}
+          >
+            Sign In
+          </Button>
+        </div>
       </div>
     </div>
   );
