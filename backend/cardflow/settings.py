b098aef0a86adbad5b790c8f6a90d2dfb260bb7b
Simeon Todorov <59865649+dinocom33@users.noreply.github.com>
2023-11-12 12:48:52 +0200

    Update settings.py

--- backend/cardflow/settings.py
+++ backend/cardflow/settings.py
@@ -126,7 +126,7 @@ REST_FRAMEWORK = {
     'TITLE': 'Cardflow APP  ',
     'DESCRIPTION': 'This is the API for the Cardflow APP',
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
-    # 'DEFAULT_FILTER_BACKENDS': (
-    #     'django_filters.rest_framework.DjangoFilterBackend',
-    # ),
+    'DEFAULT_FILTER_BACKENDS': (
+        'django_filters.rest_framework.DjangoFilterBackend',
+    ),
 }
