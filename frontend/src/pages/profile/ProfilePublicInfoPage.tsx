004309eab93c98181fca77d11f0a554bd9fbeddd
RyotaMitaraiWeb <titanium5624@gmail.com>
2023-11-12 23:03:55 +0200

    create ProfilePage component

--- frontend/src/pages/profile/ProfilePublicInfoPage.tsx
+++ frontend/src/pages/profile/ProfilePublicInfoPage.tsx
@@ -1,19 +1,15 @@
-import PageHeader from '../../components/pageHeader/PageHeader';
-import ProfileNavigation from '../../components/profile/ProfileNavigation';
 import ProfilePublicData from '../../components/profile/ProfilePublicData';
 import ProfileMarketActivity from '../../components/profile/profileMarketActivity/ProfileMarketActivity';
+import ProfilePage from '../../components/profile/profilePage/ProfilePage';
+
 function ProfilePublicInfoPage(): JSX.Element {
   return (
-    <section>
-      <PageHeader heading="Account"></PageHeader>
-      <div className="block text-center p-8 lg:text-left lg:grid lg:grid-cols-[1fr_3fr] lg:p-24">
-        <ProfileNavigation />
-        <div className="flex flex-col gap-2">
-          <ProfilePublicData />
-          <ProfileMarketActivity />
-        </div>
+    <ProfilePage>
+      <div className="flex flex-col gap-2">
+        <ProfilePublicData />
+        <ProfileMarketActivity />
       </div>
-    </section>
+    </ProfilePage>
   );
 }
 
