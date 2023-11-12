8c3cf0996f4a4fdb331ae3f89acf321c08690089
Zhivko Tringov <zhivkotringov@gmail.com>
2023-11-12 21:19:49 +0200

    Creating superuser when executing docker-compose.yml

---
+++ backend/entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+echo "Waiting for postgres..."
+
+while ! nc -z 'postgres' 5432; do
+  echo "Zzz"
+  sleep 0.5
+done
+
+echo "PostgreSQL started"
+
+python manage.py flush --no-input
+python manage.py migrate
+python manage.py createsuperuser --no-input
+
+
+
+python manage.py runserver 0.0.0.0:8000
