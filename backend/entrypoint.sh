0d5e5fdc466b2207d56f8fc02ad0280d458dc08b
Zhivko Tringov <zhivkotringov@gmail.com>
2023-11-13 01:53:06 +0200

    Fix creating superuser when executing docker-compose.yml

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+if [ -f .env ]; then
+  export $(dotenv list -e)
+fi
+
+
 echo "Waiting for postgres..."
 
 while ! nc -z 'postgres' 5432; do
@@ -9,11 +14,13 @@ done
 
 echo "PostgreSQL started"
 
+
 python manage.py flush --no-input
 python manage.py migrate
 
 
-if [ -z "$(python manage.py shell -c 'from django.contrib.auth.models import User; print(User.objects.filter(username="your_superuser_username").exists())')" ]; then
+# Check if the superuser already exists using the value from .env
+if [ -z "$(python manage.py shell -c 'from django.contrib.auth.models import User; print(User.objects.filter(username=os.environ.get("DJANGO_SUPERUSER_USERNAME")).exists())')" ]; then
   echo "Creating superuser..."
   python manage.py createsuperuser --no-input
 else
