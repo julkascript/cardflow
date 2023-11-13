e4326ade19d291870401babfff2893688cdccb71
Zhivko Tringov <zhivkotringov@gmail.com>
2023-11-13 02:37:26 +0200

    Fix settings.py

--- backend/cardflow/settings.py
+++ backend/cardflow/settings.py
@@ -73,7 +73,7 @@ DATABASES = {
         "NAME": os.getenv("POSTGRES_DB"),
         "USER": os.getenv("POSTGRES_USER"),
         "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
-        "HOST": os.getenv("POSTGRES_HOST"),
+        "HOST": "postgres",
         "PORT": os.getenv("POSTGRES_PORT") 
     }
 }
