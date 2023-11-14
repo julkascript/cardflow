1404326e6e5fd99270c9f7c4a7cf100eee880617
Zhivko Tringov <zhivkotringov@gmail.com>
2023-11-14 23:35:05 +0200

    Fix issues

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 
+# shellcheck disable=SC1073
+if [ -f .env ]; then
+  export $(dotenv list -e)
+  fi
 
 echo "Waiting for postgres..."
 
-while ! nc -z $POSTGRES_HOST $POSTGRES_PORT; do
+while ! nc -z 'postgres' $POSTGRES_PORT; do
   echo "Zzz"
   sleep 0.5
 done
@@ -14,12 +18,10 @@ echo "PostgreSQL started"
 python manage.py flush --no-input
 python manage.py migrate
 
-if [ -z "$(python manage.py shell -c "from django.contrib.auth.models import User; print(User.objects.filter(username='$DJANGO_SUPERUSER_USERNAME').exists())")" ]; then
+if [ "$(python manage.py shell -c "from django.contrib.auth.models import User; print(User.objects.filter(username='admin').exists())")" ]; then
   echo "Creating $DJANGO_SUPERUSER_USERNAME superuser..."
-  python manage.py createsuperuser --no-input \
-    --username="$DJANGO_SUPERUSER_USERNAME" \
-    --email="$DJANGO_SUPERUSER_EMAIL" \
-    --password="$DJANGO_SUPERUSER_PASSWORD"
+  "$(python manage.py shell -c "from django.contrib.auth.models import User; User.objects.create_superuser('admin', password='admin')")"
+  echo "$DJANGO_SUPERUSER_USERNAME superuser is created"
 else
   echo "$DJANGO_SUPERUSER_USERNAME superuser already exists. Skipping creation."
 fi
