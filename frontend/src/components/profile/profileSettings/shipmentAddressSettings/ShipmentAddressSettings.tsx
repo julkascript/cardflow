3807c55843310907149b48af905ff723ce06f024
RyotaMitaraiWeb <titanium5624@gmail.com>
2023-11-14 23:21:01 +0200

    create ShipmentAddressSettings component

---
+++ frontend/src/components/profile/profileSettings/shipmentAddressSettings/ShipmentAddressSettings.tsx
@@ -0,0 +1,37 @@
+import { ChangeEvent, useState } from 'react';
+import PageSection from '../../../pageSection/PageSection';
+import ProfileSectionFooter from '../../profileSectionFooter/ProfileSectionFooter';
+import { Button, TextField } from '@mui/material';
+
+function ShipmentAddressSettings(): JSX.Element {
+  const [address, setAddress] = useState('');
+
+  function handleAddressChange(event: ChangeEvent<HTMLInputElement>) {
+    event.preventDefault();
+    const value = event.target.value;
+    setAddress(value);
+  }
+
+  return (
+    <PageSection>
+      <div className="pt-4 pb-4 lg:pl-12 lg:pr-12">
+        <h2 className="font-bold mb-4">Default shipment address</h2>
+        <p>Enter the default shipment address that will get pre-filled.</p>
+        <TextField value={address} onChange={handleAddressChange} size="small" />
+      </div>
+      <ProfileSectionFooter>
+        <p>You can always change the address per purchase in the checkout section.</p>
+        <Button
+          disabled={address === ''}
+          color="primary"
+          variant="contained"
+          className="inline-block"
+        >
+          Save
+        </Button>
+      </ProfileSectionFooter>
+    </PageSection>
+  );
+}
+
+export default ShipmentAddressSettings;
