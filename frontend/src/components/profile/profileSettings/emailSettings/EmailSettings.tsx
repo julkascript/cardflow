b1961c48059a77531e52668f2ed08187107cbc2b
RyotaMitaraiWeb <titanium5624@gmail.com>
2023-11-14 23:09:56 +0200

    create EmailSettings component

---
+++ frontend/src/components/profile/profileSettings/emailSettings/EmailSettings.tsx
@@ -0,0 +1,35 @@
+import { Button, TextField } from '@mui/material';
+import PageSection from '../../../pageSection/PageSection';
+import ProfileSectionFooter from '../../profileSectionFooter/ProfileSectionFooter';
+import { ChangeEvent, useState } from 'react';
+
+function EmailSettings(): JSX.Element {
+  const [email, setEmail] = useState('');
+  const emailPattern = /^.+@.+$/gim;
+
+  const isInvalid = !emailPattern.test(email);
+
+  function handleEmailChange(event: ChangeEvent<HTMLInputElement>) {
+    event.preventDefault();
+    const value = event.target.value;
+    setEmail(value);
+  }
+
+  return (
+    <PageSection>
+      <div className="pt-4 pb-4 lg:pl-12 lg:pr-12">
+        <h2 className="font-bold mb-4">Email</h2>
+        <p>Please enter your email address.</p>
+        <TextField value={email} onChange={handleEmailChange} size="small" />
+      </div>
+      <ProfileSectionFooter>
+        <p>We will email you to verify the change.</p>
+        <Button disabled={isInvalid} color="primary" variant="contained" className="inline-block">
+          Save
+        </Button>
+      </ProfileSectionFooter>
+    </PageSection>
+  );
+}
+
+export default EmailSettings;
