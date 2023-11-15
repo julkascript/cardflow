517dbfbad1c130f7ce9ecacdd0f34359519c2ca6
Yulian Prodanov <julianprodanov@icloud.com>
2023-11-15 23:56:13 +0200

    remove unused env var

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -19,11 +19,11 @@ python manage.py flush --no-input
 python manage.py migrate
 
 if [ "$(python manage.py shell -c "from django.contrib.auth.models import User; print(User.objects.filter(username='admin').exists())")" ]; then
-  echo "Creating $DJANGO_SUPERUSER_USERNAME superuser..."
+  echo "Creating superuser..."
   "$(python manage.py shell -c "from django.contrib.auth.models import User; User.objects.create_superuser('admin', password='admin')")"
-  echo "$DJANGO_SUPERUSER_USERNAME superuser is created"
+  echo "Superuser is created"
 else
-  echo "$DJANGO_SUPERUSER_USERNAME superuser already exists. Skipping creation."
+  echo "Superuser already exists. Skipping creation."
 fi
 
 python manage.py runserver 0.0.0.0:8000
