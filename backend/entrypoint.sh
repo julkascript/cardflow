7dbe60cb7cd890530fcdf03f9e300da080d7ceb5
Milen Palachorov <feniks9511@gmail.com>
2023-11-18 01:58:52 +0200

    Fix issue #14: Superuser not created on container run

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -18,9 +18,9 @@ echo "PostgreSQL started"
 python manage.py flush --no-input
 python manage.py migrate
 
-if [ "$(python manage.py shell -c "from django.contrib.auth.models import User; print(User.objects.filter(username='admin').exists())")" ]; then
+if [ "$(python manage.py shell -c 'from accounts.models import User; print(User.objects.filter(username="admin", email="'"$PGADMIN_DEFAULT_EMAIL"'").exists())')" = "False" ]; then
   echo "Creating superuser..."
-  "$(python manage.py shell -c "from django.contrib.auth.models import User; User.objects.create_superuser('admin', password='admin')")"
+  python manage.py shell -c 'from accounts.models import User; User.objects.create_superuser("admin", "'"$PGADMIN_DEFAULT_EMAIL"'", password="'"$PGADMIN_DEFAULT_PASSWORD"'")'
   echo "Superuser is created"
 else
   echo "Superuser already exists. Skipping creation."
