e1ca68ddb5e16c690aae3cf96dcf8a0097231941
RyotaMitaraiWeb <titanium5624@gmail.com>
2023-11-20 21:32:03 +0200

    create hooks and context providers for the current user

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import Router from './router/Router.tsx';
+import { UserContextProvider } from './context/user/user.tsx';
 // import Router from './router/Router.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Router />
+    <UserContextProvider>
+      <Router />
+    </UserContextProvider>
   </React.StrictMode>,
 );
