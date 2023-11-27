ae3e48b5bde947f8e57c40b42b8a837561c88959
RyotaMitaraiWeb <titanium5624@gmail.com>
2023-11-27 14:45:16 +0200

    create YugiohCardConditionLabel component

---
+++ frontend/src/components/yugioh/YugiohCardConditionLabel.tsx
@@ -0,0 +1,27 @@
+type condition = 'poor' | 'played' | 'good' | 'excellent';
+
+const conditionColors: Record<condition, string> = {
+  poor: '#F73378',
+  played: '#FF9100',
+  good: '#00E676',
+  excellent: '#33BFFF',
+};
+
+type YugiohCardConditionLabelProps = {
+  condition: condition;
+};
+
+function YugiohCardConditionLabel(props: YugiohCardConditionLabelProps) {
+  const color = conditionColors[props.condition];
+
+  return (
+    <div
+      style={{ color, borderColor: color }}
+      className="pl-2 pr-2 border flex items-center justify-center text-center rounded-xl uppercase"
+    >
+      {props.condition}
+    </div>
+  );
+}
+
+export default YugiohCardConditionLabel;
