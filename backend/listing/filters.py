7c81ad81f04fe0a6d8fafafe711c8d1c761ae458
Milen Palachorov <feniks9511@gmail.com>
2023-12-04 16:30:10 +0200

    extended buy functionality to PATCH and PUT methods and reformated the logic

--- backend/listing/filters.py
+++ backend/listing/filters.py
@@ -1,4 +1,5 @@
 from django_filters import rest_framework as filters
+
 from .models import Listing
 
 
@@ -17,4 +18,4 @@ class ListingFilter(filters.FilterSet):
 
     class Meta:
         model = Listing
-        fields = ['is_listed']
+        fields = ['is_listed', 'is_sold']
--- backend/listing/views.py
+++ backend/listing/views.py
@@ -30,19 +30,37 @@ class ListingViewSet(viewsets.ModelViewSet):
         serializer.save(user=self.request.user)
 
     def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)
         instance = self.get_object()
 
+        request_is_sold = self.request.data.get('is_sold', False)
+
         if instance.is_sold:
+            instance.is_listed = False
+            instance.save()
             return Response({"detail": "Listing is already sold."}, status=status.HTTP_400_BAD_REQUEST)
-        if not instance.is_listed:
-            return Response({"detail": "Unlisted items cannot be sold."}, status=status.HTTP_400_BAD_REQUEST)
 
-        # Mark the listing as sold
-        instance.is_sold = True
-        instance.is_listed = False
-        instance.save()
+        if bool(request_is_sold):
+            if not instance.is_listed:
+                return Response({"detail": "Unlisted items cannot be sold."}, status=status.HTTP_400_BAD_REQUEST)
 
-        serializer = self.get_serializer(instance)
+            # Mark the listing as sold
+            instance.is_sold = True
+            instance.is_listed = False
+            instance.save()
+
+        if self.request.data.get('is_listed'):
+            self.request.data['is_listed'] = False
+
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+
+        if getattr(instance, '_prefetched_objects_cache', None):
+            # If 'prefetch_related' has been applied to a queryset, we need to
+            # forcibly invalidate the prefetch cache on the instance.
+            instance._prefetched_objects_cache = {}
+
         return Response(serializer.data)
 
     def list(self, request, *args, **kwargs):
