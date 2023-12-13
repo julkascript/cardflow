eed90f6dc72807bcd040eb20c9e9b29637ad33ca
Simeon Todorov <59865649+dinocom33@users.noreply.github.com>
2023-12-13 13:02:58 +0200

    Update views.py

--- backend/listing/views.py
+++ backend/listing/views.py
@@ -2,7 +2,6 @@ from django_filters.rest_framework import DjangoFilterBackend
 from drf_spectacular.utils import extend_schema
 from rest_framework import viewsets
 from rest_framework import permissions
-from rest_framework.decorators import action
 
 from .filters import ListingFilter
 from .models import Listing
