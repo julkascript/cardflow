fbc877fe61bebe9d434f48ed9344b1f0b87071da
RyotaMitaraiWeb <titanium5624@gmail.com>
2023-12-14 01:18:53 +0200

    update API constants with some new endpoints

--- frontend/src/constants/api.ts
+++ frontend/src/constants/api.ts
@@ -1,10 +1,25 @@
 const base = `${import.meta.env.VITE_API_URL}/api`;
 const accounts = `${base}/accounts`;
+const listing = `${base}/listing`;
+const yugioh = `${base}/yugioh`;
+const cardInSet = `${yugioh}/cards_in_set`;
 
 export const api = Object.freeze({
   accounts: {
     refresh: `${accounts}/refresh/`,
     register: `${accounts}/register/`,
     login: `${accounts}/login/`,
+  },
+  yugioh: {
+    listing: {
+      root: listing,
+
+      /** listing/{insert_your_id} */
+      id: (id: number | string) => `${listing}/${id}`,
+    },
+    cardInSet: {
+      root: cardInSet,
+      id: (id: number | string) => `${cardInSet}/${id}`,
+    },
   },
 });
