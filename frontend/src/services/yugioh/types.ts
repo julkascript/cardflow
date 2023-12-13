aa443aee580fb2693690f087d3270c7fbb6c21ac
RyotaMitaraiWeb <titanium5624@gmail.com>
2023-12-14 01:19:54 +0200

    create some types for Yugioh cards

---
+++ frontend/src/services/yugioh/types.ts
@@ -0,0 +1,66 @@
+export type condition = 'poor' | 'played' | 'good' | 'excellent';
+
+export type YugiohCardSet = {
+  id: number;
+  card_set_name: string;
+  set_code: string;
+};
+
+export type YugiohCardRarity = {
+  id: number;
+  rarity: string;
+  rarity_code: string;
+};
+
+export type YugiohCard = {
+  id: number;
+  card_name: string;
+  type: string;
+  frame_type: string;
+  description: string;
+  attack: string;
+  defense: string;
+  level: string;
+  race: string;
+  attribute: string;
+  archetype: string;
+  image: string;
+  card_in_sets: YugiohCardSet;
+};
+
+export type YugiohCardListing = {
+  id: number;
+  card: number;
+  card_name: string;
+  card_set_id: number;
+
+  /** This is the user's ID */
+  user: number;
+
+  user_name: string;
+  price: number;
+  condition: condition;
+  quantity: number;
+  is_listed: boolean;
+  is_sold: boolean;
+};
+
+export type YugiohCardInSet = {
+  id: number;
+  yugioh_card: {
+    id: number;
+    card_name: string;
+    type: string;
+    frame_type: string;
+    description: string;
+    attack: string;
+    defense: string;
+    level: string;
+    race: string;
+    attribute: string;
+    archetype: string;
+    image: string;
+  };
+  set: YugiohCardSet;
+  rarity: YugiohCardRarity;
+};
