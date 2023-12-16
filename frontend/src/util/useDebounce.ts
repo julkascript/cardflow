de750ac772d8f0e62d5385238477a0f68ac053a5
RyotaMitaraiWeb <titanium5624@gmail.com>
2023-12-16 20:27:54 +0200

    create useDebounce hook

---
+++ frontend/src/util/useDebounce.ts
@@ -0,0 +1,43 @@
+import { useEffect, useMemo, useRef } from 'react';
+
+/**
+ * @param callback the function to be executed.
+ * @param timeout expressed in miliseconds, defaults to 500
+ */
+function debounce<T extends (...args: any[]) => ReturnType<T>>(
+  callback: T,
+  timeout = 500,
+): (...args: Parameters<T>) => void {
+  let timer: ReturnType<typeof setTimeout>;
+
+  return (...args: Parameters<T>) => {
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      callback(...args);
+    }, timeout);
+  };
+}
+
+/**
+ * A hook for debouncing functions. When called, the passed callback
+ * will be called after ``timeout`` miliseconds pass, provided that the function
+ * is not called again before that.
+ * @param callback the function to be executed
+ * @param timeout expressed in miliseconds, defaults to 500
+ */
+export const useDebounce = (callback: (...args: any[]) => void, timeout = 500) => {
+  const ref = useRef(() => {});
+
+  useEffect(() => {
+    ref.current = callback;
+  }, [callback]);
+
+  const debouncedCallback = useMemo(() => {
+    const func = () => {
+      ref.current?.();
+    };
+    return debounce(func, timeout);
+  }, []);
+
+  return debouncedCallback;
+};
