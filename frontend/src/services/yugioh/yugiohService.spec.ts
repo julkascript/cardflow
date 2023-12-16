c484895a103f3352329395d63e2a7c4b4a41d758
RyotaMitaraiWeb <titanium5624@gmail.com>
2023-12-16 23:33:49 +0200

    add a method for searching cards to the yugioh service

--- frontend/src/services/yugioh/yugiohService.spec.ts
+++ frontend/src/services/yugioh/yugiohService.spec.ts
@@ -1,6 +1,6 @@
 import { describe, it, expect, vi } from 'vitest';
 import { httpService } from '../http/http';
-import { YugiohCardInSet } from './types';
+import { YugiohCard, YugiohCardInSet } from './types';
 import { yugiohService } from './yugiohService';
 
 describe('yugiohService', () => {
@@ -38,6 +38,31 @@ describe('yugiohService', () => {
 
       const result = await yugiohService.getCardInSetById(1);
       expect(result).toEqual(sampleCardInSet);
+    });
+  });
+
+  describe('searchCardsByName', () => {
+    it('returns data successfully', async () => {
+      const sampleCard: YugiohCard = {
+        id: 3,
+        card_name: 'test',
+        type: '',
+        frame_type: '',
+        description: '',
+        attack: '',
+        defense: '',
+        level: '',
+        race: '',
+        attribute: '',
+        archetype: '',
+        image: '',
+        card_in_sets: [],
+      };
+
+      vi.spyOn(httpService, 'get').mockResolvedValueOnce([sampleCard]);
+
+      const result = await yugiohService.searchCardsByName('test');
+      expect(result).toEqual([sampleCard]);
     });
   });
 });
--- frontend/src/services/yugioh/yugiohService.ts
+++ frontend/src/services/yugioh/yugiohService.ts
@@ -1,10 +1,18 @@
 import { api } from '../../constants/api';
 import { httpService } from '../http/http';
-import { YugiohCardInSet } from './types';
+import { YugiohCardInSet, YugiohCard } from './types';
 
 export const yugiohService = {
   async getCardInSetById(id: number): Promise<YugiohCardInSet> {
     const card = await httpService.get<YugiohCardInSet>(api.yugioh.cardInSet.id(id));
     return card!;
+  },
+
+  async searchCardsByName(name: string): Promise<YugiohCard[]> {
+    const cards = await httpService.get<YugiohCard[]>(api.yugioh.cards.root, {
+      card_name: name,
+    });
+
+    return cards!;
   },
 };
