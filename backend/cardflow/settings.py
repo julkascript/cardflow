92e3f1b8885e2bb293cc122ce8e28a2c6888bd01
Simeon Todorov <59865649+dinocom33@users.noreply.github.com>
2023-12-30 15:29:32 +0200

    Update settings.py

--- backend/cardflow/settings.py
+++ backend/cardflow/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         "NAME": os.getenv("POSTGRES_DB"),
         "USER": os.getenv("POSTGRES_USER"),
         "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
-        "HOST": "postgres",
+        "HOST": os.getenv("POSTGRES_HOST"),
         "PORT": os.getenv("POSTGRES_PORT"),
     }
 }
