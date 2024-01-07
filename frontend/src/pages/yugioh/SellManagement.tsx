faf3c58e51d32e94a596c6fe8c92f63161767025
RyotaMitaraiWeb <titanium5624@gmail.com>
2024-01-08 00:46:14 +0200

    create barely functional table

---
+++ frontend/src/pages/yugioh/SellManagement.tsx
@@ -0,0 +1,116 @@
+import { Checkbox, Link } from '@mui/material';
+import MarketTable from '../../components/marketTable/MarketTable';
+import { YugiohCardListing } from '../../services/yugioh/types';
+import React, { Reducer, useEffect, useReducer } from 'react';
+import { yugiohService } from '../../services/yugioh/yugiohService';
+import { useAuthenticationStatus, useCurrentUser } from '../../context/user';
+import YugiohCardConditionLabel from '../../components/yugioh/YugiohCardConditionLabel';
+
+type ListingData = {
+  listing: YugiohCardListing;
+  selected: boolean;
+};
+
+type selectReducerAction = {
+  type: 'select' | 'deselect' | 'set' | 'selectAll' | 'deselectAll';
+  index?: number;
+  checked?: boolean;
+  newListings?: ListingData[];
+};
+
+function selectReducer(
+  state: ListingData[],
+  action: selectReducerAction,
+): React.ReducerState<Reducer<typeof state, typeof action>> {
+  switch (action.type) {
+    case 'select':
+      const listingsForSelect = [...state];
+      listingsForSelect[action.index || 0].selected = true;
+      return listingsForSelect;
+    case 'deselect':
+      const listingsForDeselect = [...state];
+      listingsForDeselect[action.index || 0].selected = false;
+      return listingsForDeselect;
+    case 'set':
+      return action.newListings || [];
+    case 'selectAll':
+      return state.map((ld) => ({ ...ld, selected: true }));
+    case 'deselectAll':
+      return state.map((ld) => ({ ...ld, selected: false }));
+    default:
+      throw new Error('Unknown action');
+  }
+}
+
+function SellManagement(): JSX.Element {
+  const [data, dispatch] = useReducer(selectReducer, [] as ListingData[]);
+  const { user } = useCurrentUser();
+  const { isAuthenticated } = useAuthenticationStatus();
+
+  function handleCheck(index: number) {
+    dispatch({
+      type: data[index].selected ? 'deselect' : 'select',
+      index,
+    });
+  }
+
+  function handleCheckAll() {
+    dispatch({
+      type: data.every((d) => d.selected) ? 'deselectAll' : 'selectAll',
+    });
+  }
+
+  useEffect(() => {
+    if (isAuthenticated) {
+      yugiohService.getCardListingsByUserId(user.user_id).then((listings) => {
+        const data: ListingData[] = listings.results.map((l) => ({
+          listing: l,
+          selected: false,
+        }));
+
+        dispatch({
+          type: 'set',
+          newListings: data,
+        });
+      });
+    }
+  }, [user.user_id]);
+  return (
+    <MarketTable>
+      <thead>
+        <tr>
+          <th>
+            <Checkbox onChange={handleCheckAll} color="default" />
+          </th>
+          <th colSpan={2}>Card details</th>
+          <th>Available</th>
+          <th>Price</th>
+          <th>Listed</th>
+        </tr>
+        {data.map((ld, i) => (
+          <tr key={ld.listing.id}>
+            <td>
+              <Checkbox
+                onChange={() => {
+                  handleCheck(i);
+                }}
+                checked={data[i].selected}
+              />
+            </td>
+            <td>
+              <Link>{ld.listing.card_name}</Link>
+            </td>
+            <td>
+              <YugiohCardConditionLabel condition={ld.listing.condition} />
+            </td>
+            <td>{ld.listing.quantity}</td>
+            <td>$&nbsp;{ld.listing.price}</td>
+            <td>{ld.listing.is_listed}</td>
+          </tr>
+        ))}
+      </thead>
+    </MarketTable>
+  );
+}
+
+export default SellManagement;
