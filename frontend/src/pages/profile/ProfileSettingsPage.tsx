2d04755c2de2379964058baefe2469b2e4235159
RyotaMitaraiWeb <titanium5624@gmail.com>
2024-01-19 01:55:25 +0200

    update ProfileSettingsPage

--- frontend/src/pages/profile/ProfileSettingsPage.tsx
+++ frontend/src/pages/profile/ProfileSettingsPage.tsx
@@ -1,4 +1,3 @@
-import { useState } from 'react';
 import ProfilePage from '../../components/profile/ProfilePage';
 import AvatarSettings from '../../components/profile/profileSettings/avatarSettings/AvatarSettings';
 import DeleteAccount from '../../components/profile/profileSettings/DeleteAccount';
@@ -8,41 +7,27 @@ import UsernameSettings from '../../components/profile/profileSettings/usernameS
 import { useCurrentUser } from '../../context/user';
 import { UserAccount } from '../../services/user/types';
 import { userService } from '../../services/user/user';
-import { useEffectAfterInitialLoad } from '../../util/useEffectAfterInitialLoad';
 import { useLogout } from '../../util/useLogout';
 
 function ProfileSettingsPage(): JSX.Element {
-  const { user } = useCurrentUser();
+  const { user, setUser } = useCurrentUser();
   const logout = useLogout();
-  const [userData, setUserData] = useState<UserAccount>({
-    username: '',
-    password: '',
-    email: '',
-    first_name: null,
-    last_name: null,
-    phone_number: null,
-    city: null,
-    shipping_address: null,
-    avatar: null,
-  });
 
-  function updateAccount(section: keyof UserAccount, data: string) {
+  async function updateAccount(section: keyof UserAccount, data: string) {
     const payload: Partial<UserAccount> = { [section]: data };
-    return userService.updateUser(user.user_id, payload).then(setUserData);
+    return userService.updateUser(user.user_id, payload).then((data) => {
+      setUser({ user_id: user.user_id, ...data });
+    });
   }
 
   function updateAndLogout(field: 'username' | 'email', value: string) {
     updateAccount(field, value).then(logout);
   }
 
-  useEffectAfterInitialLoad(() => {
-    userService.getUser(user.user_id).then(setUserData);
-  }, []);
-
   return (
     <ProfilePage className="bg-[#F5F5F5]">
       <div className="flex flex-col gap-8">
-        <AvatarSettings avatar={userData?.avatar?.toString() || ''} />
+        <AvatarSettings avatar={user.avatar?.toString() || ''} />
         <UsernameSettings
           onSubmit={(u) => updateAndLogout('username', u)}
           key={user.username + '1'}
