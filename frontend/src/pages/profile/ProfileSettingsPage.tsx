e4a943c5ff1ccef7bb58209701d04fc441f2676f
RyotaMitaraiWeb <titanium5624@gmail.com>
2024-01-18 02:23:33 +0200

    implement changing username functionality

--- frontend/src/pages/profile/ProfileSettingsPage.tsx
+++ frontend/src/pages/profile/ProfileSettingsPage.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import ProfilePage from '../../components/profile/ProfilePage';
 import AvatarSettings from '../../components/profile/profileSettings/avatarSettings/AvatarSettings';
 import DeleteAccount from '../../components/profile/profileSettings/DeleteAccount';
@@ -8,10 +8,12 @@ import UsernameSettings from '../../components/profile/profileSettings/usernameS
 import { useCurrentUser } from '../../context/user';
 import { UserAccount } from '../../services/user/types';
 import { userService } from '../../services/user/user';
+import { useEffectAfterInitialLoad } from '../../util/useEffectAfterInitialLoad';
+import { useLogout } from '../../util/useLogout';
 
 function ProfileSettingsPage(): JSX.Element {
   const { user } = useCurrentUser();
-
+  const logout = useLogout();
   const [userData, setUserData] = useState<UserAccount>({
     username: '',
     password: '',
@@ -24,15 +26,28 @@ function ProfileSettingsPage(): JSX.Element {
     avatar: null,
   });
 
-  useEffect(() => {
+  function updateAccount(section: keyof UserAccount, data: string) {
+    const payload: Partial<UserAccount> = { [section]: data };
+    return userService.updateUser(user.user_id, payload).then(setUserData);
+  }
+
+  function updateUsername(username: string) {
+    updateAccount('username', username).then(logout);
+  }
+
+  useEffectAfterInitialLoad(() => {
     userService.getUser(user.user_id).then(setUserData);
   }, []);
 
   return (
     <ProfilePage className="bg-[#F5F5F5]">
       <div className="flex flex-col gap-8">
-        <AvatarSettings avatar={userData?.avatar} />
-        <UsernameSettings key={user.username + '1'} username={user.username} />
+        <AvatarSettings avatar={userData?.avatar?.toString() || ''} />
+        <UsernameSettings
+          onSubmit={updateUsername}
+          key={user.username + '1'}
+          username={user.username}
+        />
         <EmailSettings key={user.email + '2'} email={user.email} />
         <ShipmentAddressSettings />
         <DeleteAccount />
