36fc01d0d2726aba92c9a5fdbd41f5d153ba96d6
Simeon Todorov <59865649+dinocom33@users.noreply.github.com>
2024-01-21 15:55:03 +0200

    Fixed issue in frontend PR

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -9,10 +9,11 @@ router = DefaultRouter()
 app_name = 'accounts'
 
 router.register('register', RegistrationView, basename='register')
+router.register('user', UserUpdateView, basename='user')
 
 urlpatterns = [
     path('', include(router.urls)),
     path('login/', MyTokenObtainPairView.as_view(), name='login'),
     path('refresh/', TokenRefreshView.as_view(), name='refresh'),
-    path('user/<int:pk>/', UserUpdateView.as_view(), name='user_update'),
+    # path('user/<int:pk>/', UserUpdateView.as_view(), name='user_update'),
 ]
--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,14 +1,19 @@
 from django.contrib.auth import get_user_model
+from django_filters.rest_framework import DjangoFilterBackend
 from drf_spectacular.utils import extend_schema
-from rest_framework import viewsets, generics, serializers, status
+from rest_framework import viewsets, generics, serializers, status, permissions
 from rest_framework.decorators import action
-from rest_framework.exceptions import PermissionDenied
+from rest_framework.exceptions import PermissionDenied, ValidationError
 from rest_framework.parsers import MultiPartParser, FormParser, JSONParser
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
 from rest_framework.response import Response
 from rest_framework_simplejwt.views import TokenObtainPairView
 
 from accounts.serializers import RegistrationSerializer, MyTokenObtainPairSerializer, UpdateUserSerializer
+
+from accounts.filters import UserFilter
+
+from accounts.permissions import IsOwnerOfObject
 
 User = get_user_model()
 
@@ -38,38 +43,35 @@ class RegistrationView(viewsets.ModelViewSet):
 
 
 @extend_schema(tags=['Accounts'])
-class UserUpdateView(generics.RetrieveUpdateDestroyAPIView):
+class UserUpdateView(viewsets.ModelViewSet):
     """
     View for get or update the user personal information and user delete \n
         - API endpoint: /api/accounts/user/<int:pk>/
     """
 
     queryset = User.objects.all()
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticatedOrReadOnly, IsOwnerOfObject]
     serializer_class = UpdateUserSerializer
     parser_classes = [JSONParser, MultiPartParser, FormParser]
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = UserFilter
+    http_method_names = ['get', 'put', 'patch', 'delete']
+
+    def list(self, request, *args, **kwargs):
+
+        queryset = self.filter_queryset(self.get_queryset())
+        user_name = self.request.query_params.get('username', None)
+
+        if user_name is None:
+            raise ValidationError('Username is required')
+
+        queryset = queryset.filter(username=user_name)
+
+        serializer = self.get_serializer(queryset, many=True)
+
+        return Response({'username': serializer.data[0]['username'], 'avatar': serializer.data[0]['avatar']})
 
     def retrieve(self, request, *args, **kwargs):
-        """
-        View for update the user personal information and user delete \n
-            - API endpoint for get user information. : /api/accounts/user/<int:pk>/
-        """
-        user = self.get_object()
-
-        if user.pk != request.user.pk:
-            raise PermissionDenied({'authorize': 'You dont have permission for this user.'})
-
-        serializer = self.get_serializer(user)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance)
         return Response(serializer.data)
-
-    def delete(self, request, *args, **kwargs):
-        """
-        View for update the user personal information and user delete \n
-            - API endpoint for user delete. : /api/accounts/user/<int:pk>/
-        """
-        user = self.get_object()
-
-        if user.pk != request.user.pk:
-            raise PermissionDenied({'authorize': 'You dont have permission for this user.'})
-
-        return self.destroy(request, *args, **kwargs)
