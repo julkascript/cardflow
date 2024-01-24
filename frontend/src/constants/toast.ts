98ad95eb048d3170f04f1ebfa459a148cd3332fd
RyotaMitaraiWeb <titanium5624@gmail.com>
2024-01-24 02:42:50 +0200

    create toast messages

---
+++ frontend/src/constants/toast.ts
@@ -0,0 +1,13 @@
+export const toastMessages = {
+  success: {
+    login: 'Logged in successfully!',
+    register: 'Successful registration!',
+    logout: 'Successfully logged out!',
+  },
+  error: {
+    serverError: 'Something went wrong, please try again later!',
+    notLoggedIn: 'You must be logged in to perform this action!',
+    noPermission: 'You do not have permissions to perform this action!',
+    pageDoesNotExist: 'The page you are looking for does not exist',
+  },
+};
