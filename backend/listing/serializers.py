f31ebb7117fadcfacf6dae0aa1497b23e3d8fe8a
milensski <feniks9511@gmail.com>
2024-01-28 22:24:50 +0200

    adding card_in_set serializer field in the listing

--- backend/listing/serializers.py
+++ backend/listing/serializers.py
@@ -1,3 +1,4 @@
+from yugioh.serializers import YugiohCardInSetSerializer
 from rest_framework import serializers
 from .models import Listing
 
@@ -5,11 +6,12 @@ from .models import Listing
 class ListingSerializer(serializers.ModelSerializer):
     card_name = serializers.CharField(read_only=True, source='card.yugioh_card')
     card_set_id = serializers.IntegerField(read_only=True, source='card.set.id')
+    card_in_set = YugiohCardInSetSerializer(read_only=True, source='card')
     user_name = serializers.CharField(read_only=True, source='user.username')
 
     class Meta:
         model = Listing
-        fields = ['id', 'card', 'card_name', 'card_set_id', 'user', 'user_name', 'price', 'condition', 'quantity',
+        fields = ['id', 'card', 'card_name', 'card_set_id', 'card_in_set','user', 'user_name', 'price', 'condition', 'quantity',
                   'is_listed', 'is_sold']
         read_only_fields = ['id', 'user']
         ordering_fields = ['id']
