8614fc800714c19f9fa519d4e9422f4f5e0386be
Milen Palachorov <feniks9511@gmail.com>
2024-02-01 09:52:04 +0200

    reformating some paths and urls to the domain
    making script to run build command on frontend container

--- backend/cardflow/settings.py
+++ backend/cardflow/settings.py
@@ -19,7 +19,7 @@ try:
 except:
     DEBUG = False
 
-ALLOWED_HOSTS = ["34.159.169.126", "localhost", 'web.com']
+ALLOWED_HOSTS = ["cardflow.market"]
 
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = False
@@ -44,7 +44,6 @@ INSTALLED_APPS = [
     "drf_spectacular",
     "order",
     "cart",
-
 
     # MUST be the last app
     'django_cleanup.apps.CleanupConfig',
@@ -126,9 +125,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 MEDIA_URL = "media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
+STATIC_ROOT = BASE_DIR / "static_files"
+STATICFILES_DIRS = [BASE_DIR / "static/", ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
