cdd273b37aa88617c29404410441be0735b01fb3
Milen <feniks9511@gmail.com>
2024-03-05 16:03:17 +0200

    testing commit

--- backend/order/serializers.py
+++ backend/order/serializers.py
@@ -1,5 +1,5 @@
 from django.contrib.auth import get_user_model
-from rest_framework import serializers
+from rest_framework import serializers, exceptions
 
 from accounts.serializers import UserSerializer
 from listing.serializers import ListingSerializer
@@ -28,6 +28,26 @@ class OrderSerializer(serializers.ModelSerializer):
 
     def get_order_id(self, obj):
         return obj.id
+
+    def update(self, instance, validated_data):
+
+        user = self.context['request'].user
+        sender_user = instance.sender_user
+        receiver_user = instance.receiver_user
+
+        if 'status' in validated_data and validated_data['status'] == 'rejected':
+
+            if user != sender_user or user != receiver_user:
+                raise exceptions.PermissionDenied("You don't have permission to update this order status.")
+
+            for order_item in instance.orderitem_set.all():
+                order_listing = order_item.listing
+                order_listing.quantity += order_item.quantity
+                order_item.is_listed = True
+                order_listing.is_sold = False
+                order_listing.save()
+
+        return super().update(instance, validated_data)
 
 
 class FeedbackAndRatingSerializer(serializers.ModelSerializer):
@@ -58,7 +78,8 @@ class FeedbackAndRatingSerializer(serializers.ModelSerializer):
         if receiver_user != related_order.sender_user:
             raise serializers.ValidationError('The user you are trying to give feedback is not the seller of the order')
 
-        if attrs['related_order'].id in [order['related_order'] for order in FeedbackAndRating.objects.values('related_order')]:
+        if attrs['related_order'].id in [order['related_order'] for order in
+                                         FeedbackAndRating.objects.values('related_order')]:
             raise serializers.ValidationError('User already gave feedback for this order')
 
         return attrs
