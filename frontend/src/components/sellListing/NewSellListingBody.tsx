fd340ea83fed9cf590a3f30767f80d26edf1901b
RyotaMitaraiWeb <titanium5624@gmail.com>
2024-04-30 18:16:05 +0300

    make the placeholder card more responsive

--- frontend/src/components/sellListing/NewSellListingBody.tsx
+++ frontend/src/components/sellListing/NewSellListingBody.tsx
@@ -3,8 +3,8 @@ import SearchField from '../searchField/SearchField';
 
 const NewListingBody: React.FC = () => {
   return (
-    <div className="m-48 mt-8 p-20 pt-8 flex items-center bg-white border border-gray-200">
-      <div className="flex justify-around items-center w-[162px] h-[237px] bg-neutral-50 border border-stone-500"></div>
+    <div className="m-48 w-11/12 lg:w-5/6 mx-auto mt-8 p-8 flex items-center border border-gray-200">
+      <div className="w-1/2 lg:w-1/4 pb-[50%] lg:pb-[30%] bg-neutral-50 border border-stone-500"></div>
       <div className="self-start ml-12">
         <SearchField isListing={true} />
       </div>
