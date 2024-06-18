73749ae9d3d177b908e6e61c641162d61797dd36
RyotaMitaraiWeb <titanium5624@gmail.com>
2024-06-19 01:53:08 +0300

    translate the FAQ page

--- frontend/src/constants/faq.tsx
+++ frontend/src/constants/faq.tsx
@@ -1,6 +1,64 @@
 import { Link } from '@mui/material';
 
 /**
+ * This array provides the translation keys which are then used to render the FAQ page.
+ *
+ * In additition to providing translation keys, you can also define components and tags
+ * which will be interpolated in the answer associated with the key. This can be used
+ * for formatting and other more complex markup which you may want to include in the answers.
+ *
+ * **Note:** each block of text surrounded by <p> tags (in the translation itself) will be interpolated
+ * by the FAQ page itself, you do not have to provide that yourself
+ */
+export const faq: readonly FAQ[] = Object.freeze([
+  {
+    translationKey: 'cardflowFree',
+  },
+  {
+    translationKey: 'createListing',
+    components: {
+      linkToListingPage: <Link href="/sell/new" />,
+      linkToRegister: <Link href="/register" />,
+    },
+  },
+  {
+    translationKey: 'trading',
+  },
+  {
+    translationKey: 'deleteAccount',
+  },
+  {
+    translationKey: 'achievements',
+  },
+  {
+    translationKey: 'notSatisfiedWithOrder',
+  },
+  {
+    translationKey: 'report',
+    components: {
+      linkToContact: <Link href="/about/contact" />,
+    },
+  },
+  {
+    translationKey: 'listingExpiration',
+  },
+  {
+    translationKey: 'reportingABug',
+    components: {
+      linkToContact: <Link href="/about/contact" />,
+      linkToGitHub: (
+        <Link href="https://github.com/julkascript/cardflow/issues/new" target="_blank" />
+      ),
+    },
+  },
+]);
+
+type FAQ = {
+  translationKey: string;
+  components?: object;
+};
+
+/**
  * This array is used to render the accordions in the FAQ page.
  *
  * The answers can be defined as any valid React node (such as strings or HTML tags).
--- frontend/src/pages/about/FAQ.tsx
+++ frontend/src/pages/about/FAQ.tsx
@@ -4,37 +4,51 @@ import PageSection from '../../components/PageSection';
 import CardflowTabs from '../../components/sellListing/CardflowTabs';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import { faq } from '../../constants/faq';
+import { useTranslation, Trans } from 'react-i18next';
 
 function FAQ(): JSX.Element {
+  const { t } = useTranslation('about');
   const breadcrumbNavigation: BreadcrumbLink[] = [
     {
       href: '/about',
-      text: 'About',
+      text: t('common.about'),
     },
   ];
   return (
     <section className="bg-[#F5F5F5] min-h-[100vh] pb-4">
       <CardflowTabs />
-      <BreadcrumbNavigation links={breadcrumbNavigation} heading="FAQ" />
+      <BreadcrumbNavigation links={breadcrumbNavigation} heading={t('faq.title')} />
       <PageSection className="mt-4 w-11/12 py-4 lg:w-4/5 mx-auto">
         <h2 className="text-2xl text-center lg:text-left lg:px-12 px-8 pt-4 pb-8 text-bold">
-          Frequently asked questions
+          {t('faq.title')}
         </h2>
         <div className="w-11/12 mx-auto">
-          {faq.map((item) => (
-            <Accordion className="mx-auto" sx={{ backgroundColor: '#FBFBFB' }} key={item.question}>
-              <AccordionSummary
-                id={`${item.question}-header`}
-                aria-controls={`${item.question}-content`}
-                expandIcon={<ExpandMoreIcon />}
+          {faq.map((item) => {
+            const components = item.components || {};
+
+            return (
+              <Accordion
+                className="mx-auto"
+                sx={{ backgroundColor: '#FBFBFB' }}
+                key={item.translationKey}
               >
-                {item.question}
-              </AccordionSummary>
-              <AccordionDetails>
-                {typeof item.answer === 'string' ? <p>{item.answer}</p> : item.answer}
-              </AccordionDetails>
-            </Accordion>
-          ))}
+                <AccordionSummary
+                  id={`${item.translationKey}-header`}
+                  aria-controls={`${item.translationKey}-content`}
+                  expandIcon={<ExpandMoreIcon />}
+                >
+                  {t(`faq.questions.${item.translationKey}.question`)}
+                </AccordionSummary>
+                <AccordionDetails>
+                  <Trans
+                    i18nKey={`faq.questions.${item.translationKey}.answer`}
+                    t={t}
+                    components={{ p: <p className="mb-4 last:mb-0" />, ...components }}
+                  ></Trans>
+                </AccordionDetails>
+              </Accordion>
+            );
+          })}
         </div>
       </PageSection>
     </section>
