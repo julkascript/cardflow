cd7656a9426202c58926dc398c36159fd5cea3e5
RyotaMitaraiWeb <titanium5624@gmail.com>
2024-06-20 01:02:37 +0300

    remove legacy messages

--- frontend/src/constants/validationErrorMessages/password.ts
+++ frontend/src/constants/validationErrorMessages/password.ts
@@ -1,14 +1,4 @@
-import { passwordValidationRules } from '../validationRules/password';
-
 export const passwordValidationErrorMessages = Object.freeze({
   maxLength: 'password.maxLength',
   minLength: 'password.minLength',
-});
-
-/**
- * @deprecated
- */
-export const legacyPasswordValidationErrorMessages = Object.freeze({
-  maxLength: `Password must be no longer than ${passwordValidationRules.maxLength} characters`,
-  minLength: 'Password is required',
 });
--- frontend/src/constants/validationErrorMessages/username.ts
+++ frontend/src/constants/validationErrorMessages/username.ts
@@ -1,16 +1,5 @@
-import { usernameValidationRules } from '../validationRules/username';
-
 export const usernameValidationErrorMessages = Object.freeze({
   maxLength: 'username.maxLength',
   minLength: 'username.minLength',
   pattern: 'username.pattern',
-});
-
-/**
- * @deprecated
- */
-export const legacyUsernameValidationErrorMessages = Object.freeze({
-  maxLength: `Username must be no longer than ${usernameValidationRules.maxLength} characters`,
-  minLength: 'Username is required',
-  pattern: 'The username contains an invalid character',
 });
