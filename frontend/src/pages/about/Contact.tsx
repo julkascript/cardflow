cfb6682933b9fcf295e0c22a3700539c0798d430
RyotaMitaraiWeb <titanium5624@gmail.com>
2024-06-19 23:08:51 +0300

    translate the contact page

--- frontend/src/pages/about/Contact.tsx
+++ frontend/src/pages/about/Contact.tsx
@@ -9,11 +9,13 @@ import { contactService } from '../../services/contact/contact';
 import { toastMessages } from '../../constants/toast';
 import { useNavigate } from 'react-router-dom';
 import { useToast } from '../../util/useToast';
+import { Trans, useTranslation } from 'react-i18next';
 
 function Contact(): JSX.Element {
   const [email, setEmail] = useState('');
   const [message, setMessage] = useState('');
   const toast = useToast();
+  const { t } = useTranslation('about');
 
   const navigate = useNavigate();
 
@@ -28,7 +30,7 @@ function Contact(): JSX.Element {
   const breadcrumbNavigation: BreadcrumbLink[] = [
     {
       href: '/about',
-      text: 'About',
+      text: t('common.about'),
     },
   ];
 
@@ -67,17 +69,14 @@ function Contact(): JSX.Element {
   return (
     <section className="bg-[#f5f5f5] min-h-[100vh] pb-4">
       <CardflowTabs />
-      <BreadcrumbNavigation links={breadcrumbNavigation} heading="Contact us" />
+      <BreadcrumbNavigation links={breadcrumbNavigation} heading={t('contact.title')} />
       <div className="w-5/6 mx-auto mt-4 lg:flex">
         <section
           className={`flex flex-col text-center rounded-tl-lg border-[${theme.palette.secondary.main}] rounded-tr-lg border-x border-t lg:rounded-tr-none lg:border-b lg:text-left`}
         >
           <div className="p-4 lg:p-8 pb-24">
-            <h2 className="text-3xl lg:mb-8">Reach out to us</h2>
-            <p>
-              We are open to all kind of suggestions and ideas. Bug reports are also greatly
-              appreciated.
-            </p>
+            <h2 className="text-3xl lg:mb-8">{t('common.reachOutToUs')}</h2>
+            <p>{t('contact.paragraph')}</p>
           </div>
           <div className="hidden lg:flex mt-auto">
             <div className={`w-full h-[117px] border border-${theme.palette.secondary.main}`}></div>
@@ -90,23 +89,23 @@ function Contact(): JSX.Element {
         >
           <form onSubmit={handleSubmit} className="text-center lg:text-left">
             <label className="block mb-16">
-              <h3 className="text-xl lg:mb-4">Your email</h3>
+              <h3 className="text-xl lg:mb-4">{t('contact.emailField.label')}</h3>
               <TextField
                 error={!emailIsValid && !hasNotInputtedEmail}
                 value={email}
                 onChange={handleEmailChange}
                 size="small"
                 className="w-5/6 lg:w-full"
-                placeholder="Email address"
+                placeholder={t('contact.emailField.placeholder')}
                 helperText={
                   emailIsValid || hasNotInputtedEmail
                     ? undefined
-                    : 'Please provide a valid email address!'
+                    : t('contact.emailField.errorMessage')
                 }
               />
             </label>
             <label className="block mb-16">
-              <h3 className="text-xl lg:mb-4">How can we help you?</h3>
+              <h3 className="text-xl lg:mb-4">{t('contact.messageField.label')}</h3>
               <TextField
                 error={!messageIsValid && !hasNotInputtedMessage}
                 value={message}
@@ -114,19 +113,25 @@ function Contact(): JSX.Element {
                 multiline
                 minRows={5}
                 className="w-11/12 lg:w-full"
-                placeholder="Describe your request"
+                placeholder={t('contact.messageField.placeholder')}
                 helperText={
-                  messageIsValid || hasNotInputtedMessage ? undefined : 'Please fill this field!'
+                  messageIsValid || hasNotInputtedMessage
+                    ? undefined
+                    : t('contact.messageField.errorMessage')
                 }
               />
             </label>
             <div className="flex gap-4 flex-col lg:flex-row items-center justify-between">
               <Typography className="w-full" component="p" color="text.secondary">
-                By submitting this, I confirm that I have read and understood the{' '}
-                <Typography color="text.secondary" component={Link} href="/about/privacy">
-                  Privacy Policy
-                </Typography>
-                .
+                <Trans
+                  t={t}
+                  i18nKey={'contact.privacyPolicy'}
+                  components={{
+                    linkToPrivacyPolicy: (
+                      <Typography color="text.secondary" component={Link} href="/about/privacy" />
+                    ),
+                  }}
+                ></Trans>
               </Typography>
               <Button
                 variant="contained"
@@ -139,7 +144,7 @@ function Contact(): JSX.Element {
                 }}
                 disabled={!emailIsValid || !messageIsValid}
               >
-                Submit Form
+                {t('contact.submitText')}
               </Button>
             </div>
           </form>
