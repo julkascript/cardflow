d3f2697717e6423978978a9b63b4e68568bf274a
Milen <feniks9511@gmail.com>
2024-06-30 20:38:18 +0300

    Update ProfileMarketActivity.tsx

--- frontend/src/components/profile/profileMarketActivity/ProfileMarketActivity.tsx
+++ frontend/src/components/profile/profileMarketActivity/ProfileMarketActivity.tsx
@@ -3,37 +3,30 @@ import MarketActivityTableCell from './MarketActivityTableCell';
 import { ProfilePublicDataProps } from '../ProfilePublicData';
 
 function ProfileMarketActivity(props: ProfilePublicDataProps): JSX.Element {
+  const { t } = useTranslation('account');
+
   return (
     <section>
       <table className="border-stone-300 bg-white border-spacing-0 border-separate rounded-lg border-2 w-full">
         <thead>
           <tr>
             <th colSpan={2} className="text-center lg:text-left p-4 lg:pl-12 lg:pr-12">
-              {('public.marketActivity.heading')}
+              {t('public.marketActivity.heading')}
             </th>
           </tr>
         </thead>
         <tbody>
           <tr>
-            <MarketActivityTableCell heading="Purchases" data={props.user.stats.purchases} />
-            <MarketActivityTableCell heading="Sales" data={props.user.stats.sales} />
+            <MarketActivityTableCell heading={t('public.marketActivity.purchases')} data={props.user.stats.purchases} />
+            <MarketActivityTableCell heading={t('public.marketActivity.sales')} data={props.user.stats.sales} />
           </tr>
           <tr>
-            <MarketActivityTableCell
-              heading="Sales This Month"
-              data={props.user.stats.sales_this_month}
-            />
-            <MarketActivityTableCell
-              heading="Seller Rating"
-              data={`${props.user.stats.seller_rating}/5`}
-            />
+            <MarketActivityTableCell heading={t('public.marketActivity.salesThisMonth')} data={props.user.stats.sales_this_month} />
+            <MarketActivityTableCell heading={t('public.marketActivity.saleRating')} data={`${props.user.stats.seller_rating}/5`} />
           </tr>
           <tr>
-            <MarketActivityTableCell
-              heading="Rejection Rate"
-              data={`${props.user.stats.rejection_rate}%`}
-            />
-            <MarketActivityTableCell heading="Miss Rate" data={`${props.user.stats.miss_rate}%`} />
+            <MarketActivityTableCell heading={t('public.marketActivity.rejectionRate')} data={`${props.user.stats.rejection_rate}%`} />
+            <MarketActivityTableCell heading={t('public.marketActivity.missRate')} data={`${props.user.stats.miss_rate}%`} />
           </tr>
         </tbody>
       </table>
