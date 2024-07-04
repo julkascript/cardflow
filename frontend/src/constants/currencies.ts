532d5e95bc1e82b8de0c221116da7fa555bbdd10
RyotaMitaraiWeb <titanium5624@gmail.com>
2024-07-04 17:42:11 +0300

    update currencies.ts

--- frontend/src/constants/currencies.ts
+++ frontend/src/constants/currencies.ts
@@ -1,24 +1,3 @@
-export const currencies: Readonly<Record<currency, CurrencyData>> = Object.freeze({
-  BGN: {
-    code: 'BGN',
-    exchangeRate: {
-      EUR: 0.51,
-      BGN: 1,
-    },
-  },
-  EUR: {
-    code: 'EUR',
-    exchangeRate: {
-      EUR: 1,
-      BGN: 1.96,
-    },
-  },
-});
-
-export const defaultCurrency: currency = currencies.BGN.code;
+export const defaultCurrency: currency = 'BGN';
 
 export type currency = 'BGN' | 'EUR';
-type CurrencyData = {
-  exchangeRate: Record<currency, number>;
-  code: currency;
-};
