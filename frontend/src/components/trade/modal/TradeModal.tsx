7be2943d912fe8529c0f527f56dc426eafa29a2d
RyotaMitaraiWeb <titanium5624@gmail.com>
2024-08-16 23:04:55 +0300

    remove dollar sign

--- frontend/src/components/trade/modal/TradeModal.tsx
+++ frontend/src/components/trade/modal/TradeModal.tsx
@@ -13,7 +13,7 @@ function TradeModal(props: TradeModalProps): JSX.Element {
   return (
     <Dialog fullWidth maxWidth="lg" open={props.open} onClose={props.onClose}>
       <div className="p-8">
-        {props.id ? <h2 className="font-bold text-3xl">Offer #TR-${props.id}</h2> : ''}
+        {props.id ? <h2 className="font-bold text-3xl">Offer #TR-{props.id}</h2> : ''}
         <TradeModalSection user={trade.initiator} />
         <Divider />
         <TradeModalSection user={trade.recipient} />
