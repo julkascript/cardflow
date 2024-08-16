16f95e302fc1f6274fb96a05c70cca120ce0556a
RyotaMitaraiWeb <titanium5624@gmail.com>
2024-08-16 13:03:57 +0300

    improve scrolling

--- frontend/src/components/trade/modal/section/TradeModalSection.tsx
+++ frontend/src/components/trade/modal/section/TradeModalSection.tsx
@@ -13,11 +13,11 @@ function TradeModalSection(props: TradeModalSectionProps): JSX.Element {
     trade.initiator.id === props.user.id ? trade.initiator_listing : trade.recipient_listing;
   const cash = trade.initiator.id === props.user.id ? trade.initiator_cash : trade.recipient_cash;
   return (
-    <section className="flex gap-24 items-center overflow-hidden w-full">
+    <section className="flex gap-24 items-center">
       <div className="w-[400px]">
         <TradeModalSearch user={props.user} />
       </div>
-      <div className="inline-flex gap-8 flex-shrink-1 flex-grow-1 overflow-x-auto flex-nowrap">
+      <div className="flex gap-8 flex-1 overflow-x-auto">
         {listings.map((l) => (
           <TradeModalBox user={props.user} key={l.id} listing={l} />
         ))}
