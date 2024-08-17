96a3f9f61a951b7b89842ea868f43f03defd7b8e
RyotaMitaraiWeb <titanium5624@gmail.com>
2024-08-17 03:20:08 +0300

    display an empty box when no cash has been specified

--- frontend/src/components/trade/modal/section/TradeModalSection.tsx
+++ frontend/src/components/trade/modal/section/TradeModalSection.tsx
@@ -22,8 +22,7 @@ function TradeModalSection(props: TradeModalSectionProps): JSX.Element {
         {listings.map((l) => (
           <TradeModalBox user={props.user} key={l.id} listing={l} />
         ))}
-        {cash ? <TradeModalBox user={props.user} cash={cash} /> : null}
-        {listings.length === 0 && !cash ? <TradeModalBox user={props.user} /> : null}
+        <TradeModalBox user={props.user} cash={cash || undefined} />
       </div>
     </section>
   );
