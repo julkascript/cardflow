30ee772cc63c6a23d48e8d66bf443ea41ad0163b
RyotaMitaraiWeb <titanium5624@gmail.com>
2024-08-25 15:31:27 +0300

    add a filter to search listings by username

--- backend/listing/filters.py
+++ backend/listing/filters.py
@@ -34,6 +34,12 @@ class ListingFilter(filters.FilterSet):
         label='Search by card name'
     )
 
+    username = filters.Filter(
+        field_name='user__username',
+        lookup_expr='exact',
+        label='Search by username'
+    )
+
     class Meta:
         model = Listing
         fields = ['card', 'is_listed', 'is_sold']
